3d65bedbacc1048fa5fe72decd2a926a852acf6a
psadikshya29-alt <psadikshya29@gmail.com>
2025-09-03 16:09:18 +0545

    update folder name

--- src/Pages/singlebook/Singlebook.jsx
+++ src/Pages/singlebook/Singlebook.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import Navbar from "../component/Navbar";
+import Navbar from "../components/Navbar";
 
 const SingleBook = () => {
   const { id } = useParams();
